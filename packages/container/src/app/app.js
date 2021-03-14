590abadfea08a646d0c90aa222f0301865fad2a0
itproto <itprotoltd@gmail..com>
2021-03-14 22:00:48 +0200

    [aws]Test invalidation

--- packages/container/src/app/app.js
+++ packages/container/src/app/app.js
@@ -3,7 +3,7 @@ import { MarketingApp } from '../components/marketing-app';
 
 export default () => {
     return (<div>
-        <h1>Shell here</h1>
+        <h1>Shell here!!</h1>
         <hr />
         <MarketingApp />
     </div>);
